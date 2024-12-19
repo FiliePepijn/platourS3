ca89a5753050862ea601a0a3a417beb032524a33
Pepijn Latour <pepijnlatour@gmail.com>
2024-12-19 03:14:04 +0100

    add CBR.html page; update LO.html link and configure Vite server port

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
+  server: {
+    port: 5173,
+  },
   root,
   plugins: [
     Sitemap({
