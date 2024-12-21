00ccfb57759dacd3b29a902a9eb763cc7ae7c7ae
Pepijn Latour <pepijnlatour@gmail.com>
2024-12-21 05:39:24 +0100

    add SwupScriptsPlugin for enhanced overlay functionality; refactor CSS for improved overlay styles and structure; update CBR.html for consistency

--- src/assets/js/swup.js
+++ src/assets/js/swup.js
@@ -1,6 +1,56 @@
 import Swup from 'swup';
+import SwupScriptsPlugin from '@swup/scripts-plugin';
 
 const swup = new Swup({
-    containers: ['#swup'],
+    containers: ['#swup'], 
+    plugins: [
+        new SwupScriptsPlugin({
+            head: true,
+            body: true,
+        }),
+    ],
 });
 
+//overlay function
+function initializeOverlay() {
+    console.log('Initializing overlay');
+    const img = document.querySelector('.detail-img img');
+    if (img) {
+        console.log('Image found:', img);
+        
+       //check if ther is an overlay active
+        if (!document.getElementById('overlay')) {
+
+            const overlay = document.createElement('div');
+            overlay.id = 'overlay';
+            overlay.className = 'overlay';
+            overlay.innerHTML = '<img id="overlay-img" src="" alt="Overlay Image">';
+            document.body.appendChild(overlay);
+        }
+
+        const overlay = document.getElementById('overlay');
+        const overlayImg = document.getElementById('overlay-img');
+
+        document.querySelectorAll('.detail-img img').forEach(img => {
+            img.addEventListener('click', () => {
+                overlayImg.src = img.src;
+                overlay.style.display = 'flex';
+            });
+        });
+
+        overlay.addEventListener('click', () => {
+            overlay.style.display = 'none';
+            overlayImg.src = ''; 
+        });
+    } else {
+        console.error('No image found with the selector .detail-img img');
+    }
+}
+
+
+swup.hooks.on('page:view', () => {
+    console.log('Page view detected');
+    initializeOverlay();
+});
+
+initializeOverlay();
